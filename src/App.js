0925f0e5ee404a5317bf19c284e224a13ded4d05
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-29 16:04:37 +0100

    created all pages

--- src/App.js
+++ src/App.js
@@ -4,6 +4,10 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import "./App.css";
 import Navbar2 from "./components/Navbar2/Navbar2";
 import Home from "./pages/Home/Home";
+import About from "./pages/About/About";
+import Projects from "./pages/Projects/Projects";
+import Resume from "./pages/Resume/Resume";
+import Contact from "./pages/Contact/Contact";
 
 function App() {
   return (
@@ -13,10 +17,10 @@ function App() {
         <Navbar2 />
         <Routes>
           <Route path="/" element={<Home />}></Route>
-          <Route path="/about" element={<Home />}></Route>
-          <Route path="/projects" element={<Home />}></Route>
-          <Route path="/resume" element={<Home />}></Route>
-          <Route path="/contact" element={<Home />}></Route>
+          <Route path="/about" element={<About />}></Route>
+          <Route path="/projects" element={<Projects />}></Route>
+          <Route path="/resume" element={<Resume />}></Route>
+          <Route path="/contact" element={<Contact />}></Route>
         </Routes>
       </BrowserRouter>
     </div>
