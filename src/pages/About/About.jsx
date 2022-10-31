4468777c6f7e6087fa8a58af2c248685e975649b
presh-031 <akingbulugbe.dev@gmail.com>
2022-10-31 23:23:31 +0100

    added img container to about page in desktop

--- src/pages/About/About.jsx
+++ src/pages/About/About.jsx
@@ -14,51 +14,55 @@ const About = () => {
           <span>{`{`}</span> about <span>{`}`}</span>
         </p>
       </div>
-      <div className="about-page-main">
-        <p>
-          I am a self-motivated frontend-inclined FULLSTACK developer currently working with web technologies such as{" "}
-          <span>Javascript</span>, <span>React</span>, <span>Node</span>, <span>Express</span>.
-        </p>
-        <p>
-          I have a well rounded understanding of both <span>frontend</span> and <span>backend</span> technologies
-          necessary for modern web application construction and maintenance.
-        </p>
-        <p>
-          I am a wiz at finding solutions, contributing to a collaborative work environment and ensuring employer's
-          satisfaction.
-        </p>
-        <p>
-          My goal is to gain enough experience to be regarded as one of the best software engineers in the world. Hence,
-          my main focus is currently only on what I do!
-        </p>
-        <p>Below are all the technologies I work with or I'm familiar with:</p>
-        <ul>
-          <li>Git and Github</li>
-          <li>Next</li>
-          <li>Express</li>
-          <li>Node</li>
-          <li>Javascript(ES6+)</li>
-          <li>Web flow</li>
-          <li>SCSS</li>
-          <li>Tailwind CSS</li>
-          <li>React</li>
-          <li>Mongo db</li>
-          <li>Bootstrap</li>
-        </ul>
-        <p>
-          Here's a particular one worth mentioning: <span>Google</span>.
-        </p>
-        <p>
-          Fun fact about me as a developer:
-          <p>
-            I am secretly motivated by the fact that the internet is held together by duct-tape and chewing gum. This
-            fact, keeps me going, as a developer.
-          </p>
-        </p>
-      </div>
       <div>
-        <img src="" alt="personality-img" />
-      </div>
+        <div className="about-page-main">
+          <p>
+            I am a self-motivated and detail-oriented frontend-inclined FULLSTACK developer currently working with web
+            technologies such as <span>Javascript</span>, <span>React</span>, <span>Node</span>, <span>Express</span>.
+          </p>
+          <p>
+            I have a well rounded understanding of both <span>frontend</span> and <span>backend</span> technologies
+            necessary for modern web application construction and maintenance.
+          </p>
+          <p>
+            I am a wiz at finding solutions, contributing to a collaborative work environment and ensuring employer's
+            satisfaction.
+          </p>
+          <p>
+            My goal is to gain enough experience to be regarded as one of the best software engineers in the world.
+            Hence, my main focus is currently only on what I do!
+          </p>
+          <p>Below are all the technologies I work with or I'm familiar with:</p>
+          <ul>
+            <li>HTML</li>
+            <li>CSS</li>
+            <li>SCSS</li>
+            <li>Tailwind CSS</li>
+            <li>Bootstrap</li>
+            <li>Web flow</li>
+            <li>Javascript(ES6+)</li>
+            <li>API</li>
+            <li>React</li>
+            <li>Next</li>
+            <li>Node</li>
+            <li>Express</li>
+            <li>Mongo db</li>
+            <li>npm</li>
+            <li>Git and Github</li>
+          </ul>
+          <p>
+            Here's a particular one worth mentioning: <span>Google</span>.
+          </p>
+          <p>
+            Fun fact about me as a developer:
+            <p>
+              I am secretly motivated by the fact that the internet is held together by duct-tape and chewing gum. This
+              fact, keeps me going, as a developer.
+            </p>
+          </p>
+        </div>
+        <div className="personality-img-container">{/* <img src="" alt="personality-img" /> */}</div>
+      </div>{" "}
       <FooterLinks />
     </div>
   );
